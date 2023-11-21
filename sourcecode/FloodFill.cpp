a27c34f4f1647fdc73d8bbfe5d6bf98a06eba453
Mervin Nguyen <140035006+mervinnguyen@users.noreply.github.com>
2023-11-20 23:31:45 -0800

    Add files via upload

---
+++ sourcecode/FloodFill.cpp
@@ -0,0 +1,8 @@
+/**
+ * Project: UCLA IEEE Micromouse (STM32)
+ * Names: Mervin, Peter, Colton
+ * Description: floodfill.cpp
+*/
+
+#include "FloodFill.h"
+
---
+++ sourcecode/Main.cpp
@@ -0,0 +1,42 @@
+/**
+ * Project: UCLA IEEE Micromouse (STM32)
+ * Names: Mervin, Peter, Colton
+ * Description: 
+*/
+
+#include <iostream>
+#include <string>
+
+#include "API.h"
+#include "Floodfill.h"
+
+Direction dir = NORTH;  //from floodfill header function call
+Maze maze;
+
+int center = 1;
+
+//Pass through pointer maze storing data in memory located at struct Maze
+void checkWalls(struct Maze *maze){
+    if (API::wallFront())
+        maze ->cellWalls[maze->mouse_pos.y][maze ->mouse_pos.x];
+}
+
+int main(int argc, char* argv[]) {
+    API::setColor(0, 0, 'g');   //(0,0), green
+
+    maze.mouse_pos = (Coord){0, 0};     //setting the coordinate to 0,0 facing north
+    maze.mouse_dir = NORTH;
+    
+    std::cerr << "Begin Maze Algorithm!\n" << std::endl;
+    
+    
+    //This should assign a number describing how far away the specific cell is from the center
+    for(int x = 0; x < 16; x++) 
+    {
+        for(int y = 0; y < 16; y++)
+        {
+            maze.distances[y][x] = x + y;
+        }
+    }
+
+}
