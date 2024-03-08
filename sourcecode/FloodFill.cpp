47cc35e8b1f746993154f4934ecc1d19db186e61
Peter-A-Antonio <peter.a.antonio@gmail.com>
2024-03-08 15:05:42 -0800

    Added my version of Floodfill that should be pushed to my branch

--- sourcecode/FloodFill.cpp
+++ sourcecode/FloodFill.cpp
@@ -15,55 +15,76 @@
 #include "FloodFill_DataStructures.h"
 #include "API.h"
 
-Coord queue[255];
+Coord queue[256];
 // Declaring a coord struct that        
 // will act as our queue
 //FIFO-first coordinate enqueued is the first to be dequeued in the queue
 int head = 0; //Start of the queue
 int tail = 0; //End of the queue
-
-
 int MAX_COST= 255; 
 
-for (all distance cells) //Setting every sell to arbitrary large number
+void floodfill(Maze* maze)
 {
-    distances[y][x] = MAX_COST;
+    
+
+for (int i = 0; i < 16; i++) //Setting every cell to arbitrary large number
+{   
+    for(int j = 0; j < 16; j++)
+    {
+        maze->distances[j][i] = MAX_COST;
+    }
+
 }
 
-for (all goal cells)
+for (int u = 7; u < 9; u++)
 {
-    distances[y][x] = 0; //We set the goal cells to zero since this is how far 
-                         //away the cell is from the goal aka 0
+    for(int v = 7; v < 9; v++)
+    {
+        maze->distances[v][u] = 0; //We set the goal cells to zero since this is how far 
+    }                     //away the cell is from the goal aka 0
 
 }
 
 
-for (all goal cells) //We add the goal cells to the queue given that this is veyr floodfill begins
+for () //We add the goal cells to the queue given that this is where floodfill begins
 {
 
-    queue.add(goal cell position)
-    tail++
+    Coord *goalCell;
+    goalCell->x = 7;
+    goalCell->y =7;
+
+    queue.push(goalCell);
+    tail++;
+
 }
+
+
 
 while (tail - head > 0)
 {
-    cur_pos = queue[head]; //we set the current possition to the head of the queue aka a goal cell
+    Coord cur_pos = queue[head]; //we set the current possition to the head of the queue aka a goal cell
     head++;
-    newcost = distances[pos.y][pos.x] + 1; 
+    int newcost = distances[pos.y][pos.x] + 1; 
     //increments the distance of the current position cell by one
 
-    neighborCells = getNeighborCells(cur_pos);
+    CellList* neighborCells = getNeighborCells(cur_pos);
 
     for(cell in neighborCells)
     {
         if(cell is not blocked)
         {
-            if(distances[cell.y][cell.x] > newcost)
+            if(distances[cell.y][cell.x] > newcost) //checks distance value of neighbor adjacent cells
+                                                    // if its greater than current newcost. 
             {
-                distances[cell.y][cell.x] = newcost;
+                distances[cell.y][cell.x] = newcost; //sets the new distance to newcost distance
+                queue[tail] = cell 
                 tail++
             }
         }
     }
+
+}
+
+
 
 }
