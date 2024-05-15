387fc21e69333cf05ad6fc07d85cd5b9445c4533
Peter-A-Antonio <peter.a.antonio@gmail.com>
2024-05-15 14:48:36 -0700

    timer 2 changes

--- STM32/Macrorat_STM32/Core/Src/main.c
+++ STM32/Macrorat_STM32/Core/Src/main.c
@@ -285,7 +285,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 72;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 2047;
+  htim2.Init.Period = 10000;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
